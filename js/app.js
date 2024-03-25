3cdbdd1e3e0c9b46d8ce45c5c7d482cf57e5dd23
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-25 14:50:59 +0100

    ADD: bonus

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-        message: 'Hello vue World!'
+        message: 'Hello vue World!',
+        img: 'https://www.memecreator.org/static/images/memes/4735051.jpg'
     }
     }
 }).mount('#app')
